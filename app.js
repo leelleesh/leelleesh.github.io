2edd9956d58c7f74e9eb6ae1dc5f6ef7462b7574
leelleesh <117938443+leelleesh@users.noreply.github.com>
2022-11-10 17:35:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function getData(selected_major) {
         // filter data array for the selected meal
         majors = data.filter( (item) => item.major == selected_major );  
 
-       var templateText = document.getElementById('majorTable').innerHTML;
+       var templateText = document.getElementById('cit5students.json').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
        compiledHtml = compiledTemplateText({ rows: majors })      // apply data to template
        document.getElementById('majorTable').innerHTML = compiledHtml; 
